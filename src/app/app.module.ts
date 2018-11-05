d3c04aec2e94840e5de0065608a0235df485aa2e
Victor Avila <victoravila2123@gmail.com>
2018-11-05 16:44:27 -0700

    generated: feature component (products)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// NGRX
+import { CoreModule } from './core/core.module';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductsComponent } from './products/products.component';
+import { ListComponent } from './products/list/list.component';
+import { DetailsComponent } from './products/details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ListComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/products/product.model.ts
@@ -0,0 +1,8 @@
+export interface Product {
+  id: number;
+  productName: string;
+  productCode: string;
+  price: number;
+  category: string;
+  rating: number;
+}
