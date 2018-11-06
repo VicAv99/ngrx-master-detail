97c2d65e751119be98264bbdbf5d024dfb018c42
Victor Avila <victoravila2123@gmail.com>
2018-11-05 17:57:07 -0700

    Hooked: the reducers

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { reducers } from '.';
 
 @NgModule({
   imports: [
     EffectsModule.forRoot([]),
-    StoreModule.forRoot({}),
+    StoreModule.forRoot(reducers),
     StoreDevtoolsModule.instrument({ maxAge: 10 })
   ],
   declarations: []
