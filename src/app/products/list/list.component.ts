af8b39873231187b8c4f2d2897150a7fd2445d0e
Victor Avila <victoravila2123@gmail.com>
2018-11-05 17:14:19 -0700

    created: layout

--- src/app/products/list/list.component.ts
+++ src/app/products/list/list.component.ts
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
+  products = [
+    {
+      id: 1,
+      productName: 'Hammer',
+      productCode: '#AZ123',
+      price: 4.99,
+      category: 'Maintenance',
+      rating: 3.5
+    },
+    {
+      id: 2,
+      productName: 'Fan',
+      productCode: '#FN876',
+      price: 12.75,
+      category: 'Maintenance',
+      rating: 4.8
+    },
+    {
+      id: 3,
+      productName: 'T-Shirt',
+      productCode: '#TSH150',
+      price: 2.99,
+      category: 'Clothing',
+      rating: 2
+    }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
